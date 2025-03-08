da2989bfc0961d942ad10ce9fa38d744f4662825
MohdRavishIdreesi <mohd.ravish.idreesi@gmail.com>
2025-03-08 06:41:03 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeText() {
+    document.getElementById("para").innerHTML = "Hello JavaScript!";
+    document.getElementById("cat").src = "cat2.jpg";
+}
+
+function changeText2() {
+    document.getElementById("para").innerHTML = "Here is my para from html file";
+    document.getElementById("cat").src = "cat.jpg";
+}
